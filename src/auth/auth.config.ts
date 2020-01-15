4e2a57ec7ef235759d097345d6e2f677d2ff485c
Rodolfo Rios <jrodolforios@hotmail.com>
2020-01-15 16:52:54 -0300

    Ajuste no modelo de segurança

--- src/auth/auth.config.ts
+++ src/auth/auth.config.ts
@@ -3,11 +3,11 @@ import { AuthConfig } from '../../auth-oidc/src/auth.config';
 export const authConfig: AuthConfig = {
 
     issuer: 'https://auth.mototex.cloudme.com.br',
-    clientId: 'ToDeTaxiAPI_mobile',
+    clientId: 'MotoTEXAPI_mobile',
     postLogoutRedirectUri: 'http://localhost:8100/',
     redirectUri: "http://localhost:8100/#/callback/?",
     responseType: 'code',
-    scope: 'openid profile email todetaxiapi offline_access',
+    scope: 'openid profile email mototexapi offline_access',
     dummyClientSecret: '9740e17e-9867-4477-8285-bb78485bdf2d',
 
 }
